41c31a534788b9bcdc1bbc374a0ae2a498d97099
violanerd <madalyne.cross@gmail.com>
2022-08-27 15:32:50 -0500

    added handlebars

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //const path = require('path');
 const express = require('express');
 //const session = require('express-session');
-//const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 //const helpers = require('./utils/helpers');
 
 const app = express();
@@ -23,9 +23,10 @@ const sequelize = require('./config/connection');
 // app.use(session(sess));
 
 // const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({});
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
