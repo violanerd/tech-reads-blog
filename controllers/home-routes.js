ff6b729626feda2e841378b79fe6623a35c2837d
violanerd <madalyne.cross@gmail.com>
2022-08-27 16:37:40 -0500

    landing page all posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,21 +1,18 @@
 const router = require('express').Router();
 const { User, Post, Comment} = require('../models');
-const { route } = require('./api');
 
 
-router.get('/', (req, res) => {
-    res.render('homepage');
-})
-// GET all /
-// router.get('/', async (req, res) => {
-//     try {
-//         const postData = await Post.findAll({
-//             include: [{model: User}]
-//         })
-//         res.status(200).json(postData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{model: User}]
+        })
+        const posts = postData.map(post => post.get({ plain: true}))
+        res.render('homepage', {posts});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
