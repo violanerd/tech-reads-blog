eac23fa9ef46fcc8f33dbc2b921c44a1dfbf42a9
violanerd <madalyne.cross@gmail.com>
2022-08-29 17:45:15 -0500

    hooked up edit button

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,7 +1,28 @@
-// two fetch calls
-function updatePostHandler(event){
+
+async function updatePostHandler(event){
     event.preventDefault();
-    console.log("update clicked")
+    const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1]
+    const post_content = document.querySelector('textarea[name="post-content"]').value.trim();
+    const title= document.querySelector("input[name='post-title']").value.trim();
+    if(title && post_content){
+        const response = await fetch(`/api/posts/${post_id}`,{
+            method: 'PUT',
+            body: JSON.stringify({
+                title,
+                post_content
+            }),
+            headers: {'Content-Type': 'application/json'}
+        })
+        console.log(response);
+        if (response.ok) {// make this look like lower one
+            alert("Post Updated");
+            document.location.replace('/dashboard')
+        } else {
+            alert(response.statusText)
+        }
+    } else {
+        alert("Please make sure all fields are completed")
+    }
 }
 async function deletePostHandler(event){
     event.preventDefault();
@@ -19,6 +40,5 @@ async function deletePostHandler(event){
     }
 
 } 
-// two document listeners
 document.querySelector("#delete-post").addEventListener('click', deletePostHandler)
 document.querySelector("#update-post").addEventListener('click', updatePostHandler)
