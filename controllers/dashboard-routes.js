bddff73d257719004d12d87af8e82e3bb40547fe
violanerd <madalyne.cross@gmail.com>
2022-08-29 15:37:19 -0500

    added edit post routing

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,6 +4,7 @@ const { Post, User, Comment } = require('../models');
 
 
 // code to render all the single user posts
+// /dashboard/
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
@@ -18,7 +19,24 @@ router.get('/', async (req, res) => {
 
   });
 
-// create a new post
+// edit a post /dashboard/edit/:id
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {})
+        if(!postData){
+            res.status(400).json({message: "No post found"})
+            return;
+        }
+        const post = postData.get({ plain: true });
+        res.render('edit-post', {post})
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+// delete a post /dashboard/edit/:id
+
+
+// create a new post /dashboard/new
 router.get('/new', (req, res) => {
     res.render('new-post', { loggedIn: true });
 })
